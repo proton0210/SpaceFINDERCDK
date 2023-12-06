e52780c8446f4c1bfeba485b07766f004b1decfa
Vidit Shah <vidit0210@protonmail.com>
2023-12-06 14:54:55 +0530

    Architecture

--- bin/sf.ts
+++ bin/sf.ts
@@ -1,21 +1,14 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { SfStack } from '../lib/sf-stack';
+import { App } from "aws-cdk-lib";
 
-const app = new cdk.App();
-new SfStack(app, 'SfStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
+import { DataStack } from "../src/infra/stacks/DataStack";
+import { LambdaStack } from "../src/infra/stacks/LambdaStack";
+import { ApiStack } from "../src/infra/stacks/ApiStack";
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const app = new App();
+const dataStack = new DataStack(app, "DataStack");
+const lambdaStack = new LambdaStack(app, "LambdaStack", {
+  spacesTable: dataStack.spacesTable,
+});
+new ApiStack(app, "ApiStack", {
+  spacesLambdaIntegration: lambdaStack.spacesLambdaIntegration,
 });
---
+++ src/services/spaces/PostSpaces.ts
@@ -0,0 +1,32 @@
+import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { v4 } from "uuid";
+
+export async function postSpaces(
+  event: APIGatewayProxyEvent,
+  ddbClient: DynamoDBClient
+): Promise<APIGatewayProxyResult> {
+  const randomId = v4();
+  if (!event?.body) throw new Error("No body");
+  const item = JSON.parse(event.body);
+
+  const result = await ddbClient.send(
+    new PutItemCommand({
+      TableName: process.env.TABLE_NAME,
+      Item: {
+        id: {
+          S: randomId,
+        },
+        location: {
+          S: item.location,
+        },
+      },
+    })
+  );
+  console.log(result);
+
+  return {
+    statusCode: 201,
+    body: JSON.stringify({ id: randomId }),
+  };
+}
